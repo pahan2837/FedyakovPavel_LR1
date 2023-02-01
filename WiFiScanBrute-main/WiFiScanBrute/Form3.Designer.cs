cb8edb98482478040337e561615f502d3c1b395d
Pavel <fedyakov-2002@mail.ru>
2023-02-01 11:44:26 +0500

    Коммит для лабораторной работы 1 Федяков П.Е.

---
+++ WiFiScanBrute-main/WiFiScanBrute/Form3.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace WiFiScanBrute
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.роутерыBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bDDataSet = new WiFiScanBrute.BDDataSet();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.роутерыTableAdapter = new WiFiScanBrute.BDDataSetTableAdapters.РоутерыTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.фИОDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.серийныйНомерDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.модельDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.неисправностьDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.скоростьDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.сигналDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.роутерыBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // роутерыBindingSource
+            // 
+            this.роутерыBindingSource.DataMember = "Роутеры";
+            this.роутерыBindingSource.DataSource = this.bDDataSet;
+            // 
+            // bDDataSet
+            // 
+            this.bDDataSet.DataSetName = "BDDataSet";
+            this.bDDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel|*.xlsx";
+            // 
+            // роутерыTableAdapter
+            // 
+            this.роутерыTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.фИОDataGridViewTextBoxColumn,
+            this.датаDataGridViewTextBoxColumn,
+            this.серийныйНомерDataGridViewTextBoxColumn,
+            this.модельDataGridViewTextBoxColumn,
+            this.неисправностьDataGridViewTextBoxColumn,
+            this.скоростьDataGridViewTextBoxColumn,
+            this.сигналDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.роутерыBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(845, 319);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            // 
+            // фИОDataGridViewTextBoxColumn
+            // 
+            this.фИОDataGridViewTextBoxColumn.DataPropertyName = "ФИО";
+            this.фИОDataGridViewTextBoxColumn.HeaderText = "ФИО";
+            this.фИОDataGridViewTextBoxColumn.Name = "фИОDataGridViewTextBoxColumn";
+            // 
+            // датаDataGridViewTextBoxColumn
+            // 
+            this.датаDataGridViewTextBoxColumn.DataPropertyName = "Дата";
+            this.датаDataGridViewTextBoxColumn.HeaderText = "Дата";
+            this.датаDataGridViewTextBoxColumn.Name = "датаDataGridViewTextBoxColumn";
+            // 
+            // серийныйНомерDataGridViewTextBoxColumn
+            // 
+            this.серийныйНомерDataGridViewTextBoxColumn.DataPropertyName = "Серийный номер";
+            this.серийныйНомерDataGridViewTextBoxColumn.HeaderText = "Серийный номер";
+            this.серийныйНомерDataGridViewTextBoxColumn.Name = "серийныйНомерDataGridViewTextBoxColumn";
+            // 
+            // модельDataGridViewTextBoxColumn
+            // 
+            this.модельDataGridViewTextBoxColumn.DataPropertyName = "Модель";
+            this.модельDataGridViewTextBoxColumn.HeaderText = "Модель";
+            this.модельDataGridViewTextBoxColumn.Name = "модельDataGridViewTextBoxColumn";
+            // 
+            // неисправностьDataGridViewTextBoxColumn
+            // 
+            this.неисправностьDataGridViewTextBoxColumn.DataPropertyName = "Неисправность";
+            this.неисправностьDataGridViewTextBoxColumn.HeaderText = "Неисправность";
+            this.неисправностьDataGridViewTextBoxColumn.Name = "неисправностьDataGridViewTextBoxColumn";
+            // 
+            // скоростьDataGridViewTextBoxColumn
+            // 
+            this.скоростьDataGridViewTextBoxColumn.DataPropertyName = "Скорость";
+            this.скоростьDataGridViewTextBoxColumn.HeaderText = "Скорость";
+            this.скоростьDataGridViewTextBoxColumn.Name = "скоростьDataGridViewTextBoxColumn";
+            // 
+            // сигналDataGridViewTextBoxColumn
+            // 
+            this.сигналDataGridViewTextBoxColumn.DataPropertyName = "Сигнал";
+            this.сигналDataGridViewTextBoxColumn.HeaderText = "Сигнал";
+            this.сигналDataGridViewTextBoxColumn.Name = "сигналDataGridViewTextBoxColumn";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(281, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 449);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.Text = "Таблица";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.роутерыBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private BDDataSet bDDataSet;
+        private System.Windows.Forms.BindingSource роутерыBindingSource;
+        private BDDataSetTableAdapters.РоутерыTableAdapter роутерыTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn фИОDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn серийныйНомерDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn модельDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn неисправностьDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn скоростьDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn сигналDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ WiFiScanBrute-main/WiFiScanBrute/Form3.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WiFiScanBrute
+{
+    public partial class Form3 : Form
+    {
+        private string fileName = string.Empty;
+
+
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bDDataSet.Роутеры". При необходимости она может быть перемещена или удалена.
+            this.роутерыTableAdapter.Fill(this.bDDataSet.Роутеры);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=BD.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);
+
+            // Запрос к БД
+            dbConnection.Open();
+            string query = "SELECT * FROM Роутеры";
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+            OleDbDataReader dbReader = dbCommand.ExecuteReader();
+
+            
+
+
+            dbReader.Close();
+            dbConnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Проверим количество выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверим данные в таблицы
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null ||
+                dataGridView1.Rows[index].Cells[4].Value == null ||
+                dataGridView1.Rows[index].Cells[5].Value == null ||
+                dataGridView1.Rows[index].Cells[6].Value == null ||
+                dataGridView1.Rows[index].Cells[7].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считаем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string name = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string data = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string number = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            string model = dataGridView1.Rows[index].Cells[4].Value.ToString();
+            string fail = dataGridView1.Rows[index].Cells[5].Value.ToString();
+            string speed = dataGridView1.Rows[index].Cells[6].Value.ToString();
+            string signal = dataGridView1.Rows[index].Cells[7].Value.ToString();
+
+            // Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=BD.mdb";  // строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);             // создаем соеденение
+
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();//открываем соеденение
+                string query =   "INSERT INTO Роутеры VALUES (" + id + ", '" + name + "', '" + data + "', " + number + ", '" + model + "', '" + fail + "', " + speed + ", '" + signal + "')";//строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);//команда
+
+                // Выполняем запрос
+                if (dbCommand.ExecuteNonQuery() != 1)
+                    MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+                else
+                    MessageBox.Show("Данные добавлены!", "Внимание!");
+            }
+            
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Проверим количество выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверим данные в таблицы
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null ||
+                dataGridView1.Rows[index].Cells[4].Value == null ||
+                dataGridView1.Rows[index].Cells[5].Value == null ||
+                dataGridView1.Rows[index].Cells[6].Value == null ||
+                dataGridView1.Rows[index].Cells[7].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считаем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string name = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string data = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string number = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            string model = dataGridView1.Rows[index].Cells[4].Value.ToString();
+            string fail = dataGridView1.Rows[index].Cells[5].Value.ToString();
+            string speed = dataGridView1.Rows[index].Cells[6].Value.ToString();
+            string signal = dataGridView1.Rows[index].Cells[7].Value.ToString();
+
+            // Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=BD.mdb";  // строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);             // создаем соеденение
+
+            dbConnection.Open();
+            string query = $"delete from Роутеры where Код = "+ id + "";
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+            dbCommand.ExecuteNonQuery();
+            dbConnection.Close();
+        }
+
+        
+    }
+}
